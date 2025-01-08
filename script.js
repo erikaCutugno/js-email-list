ccbc228fbc6c0e5febdad529600dadebb9582bcd
erikaCutugno <erikucciapop@gmail.com>
2025-01-08 16:23:14 +0100

    add button

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-
-const emailListElm = document.getElementById("email-list")
-
-let emailList;
-
-for(let i=0; i<10; i++){
+// FUNCTIONS
+const email = function(){
+    let emailList= "";
+    for(let i=0; i<10; i++){
     axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
     .then(function(response){
-    emailListElm.innerHTML += `<li>${response.data.response}</li>`
+    emailList += `<li>${response.data.response}</li>`
+    emailListElm.innerHTML = emailList
     })
-    
 }
+}
+// DOM ELEMENTS
+
+const emailListElm = document.getElementById("email-list")
+const btnElm = document.getElementById("btn")
+
+// EVENTS
+email();
+
+btnElm.addEventListener("click", email)
